f090a951179ff2e836ce401a61c15659d5208a0e
busraozis <busraozis1994@hotmail.com>
2025-06-29 20:57:31 +0300

    refactoring

---
+++ Extensions/ServiceRegistrationExtensions.cs
@@ -0,0 +1,101 @@
+using System.Globalization;
+using System.Net;
+using JourneyFinder.Builders;
+using JourneyFinder.Factories;
+using JourneyFinder.Filters;
+using JourneyFinder.Helpers;
+using JourneyFinder.Managers;
+using JourneyFinder.Managers.Interfaces;
+using JourneyFinder.Options;
+using JourneyFinder.Services;
+using JourneyFinder.Services.Interfaces;
+using JourneyFinder.Settings;
+using Microsoft.AspNetCore.HttpOverrides;
+
+namespace JourneyFinder.Extensions;
+
+public static class ServiceRegistrationExtensions
+{
+    public static IServiceCollection AddCustomServices(this IServiceCollection services)
+    {
+        return services
+            .AddScoped<IRedisCacheService, RedisCacheService>()
+            .AddScoped<ISessionService, SessionService>()
+            .AddScoped<IBusLocationService, BusLocationService>()
+            .AddScoped<IJourneyService, JourneyService>();
+    }
+
+    public static IServiceCollection AddCustomManagers(this IServiceCollection services)
+    {
+        return services
+            .AddScoped<ISessionManager, SessionManager>()
+            .AddScoped<IJourneyManager, JourneyManager>()
+            .AddScoped<IBusLocationManager, BusLocationManager>();
+    }
+
+    public static IServiceCollection AddCustomBuilders(this IServiceCollection services)
+    {
+        return services
+            .AddScoped<IHomeViewModelBuilder, HomeViewModelBuilder>()
+            .AddScoped<IJourneyViewModelBuilder, JourneyViewModelBuilder>();
+    }
+
+    public static IServiceCollection AddCustomFactories(this IServiceCollection services)
+    {
+        return services
+            .AddScoped<IDeviceRequestFactory, DeviceRequestFactory>()
+            .AddScoped<ICookieManager, CookieManager>();
+    }
+
+    public static IServiceCollection AddCustomFilters(this IServiceCollection services)
+    {
+        return services
+            .AddScoped<JourneyValidationFilter>();
+    }
+    
+    public static IServiceCollection AddCustomHelpers(this IServiceCollection services)
+    {
+        return services
+            .AddScoped<IRequestContextHelper,  RequestContextHelper>();
+    }
+
+    public static IServiceCollection AddCustomConfiguration(this IServiceCollection services, IConfiguration config)
+    {
+        services.Configure<ObiletApiOptions>(config.GetSection("ObiletApi"));
+        services.Configure<CookieSettings>(config.GetSection("CookieSettings"));
+        services.Configure<RequestLocalizationOptions>(options =>
+        {
+            var supportedCultures = new[] { "tr-TR", "en-EN" }
+                .Select(c => new CultureInfo(c))
+                .ToList();
+
+            options.DefaultRequestCulture = new Microsoft.AspNetCore.Localization.RequestCulture("tr-TR");
+            options.SupportedCultures = supportedCultures;
+            options.SupportedUICultures = supportedCultures;
+        });
+
+        return services;
+    }
+
+    public static IServiceCollection AddCustomLocalization(this IServiceCollection services)
+    {
+        return services.AddLocalization(options => options.ResourcesPath = "Resources");
+    }
+
+    public static IServiceCollection AddCustomRedis(this IServiceCollection services, IConfiguration config)
+    {
+        services.AddStackExchangeRedisCache(options =>
+        {
+            options.Configuration = config.GetConnectionString("Redis");
+            options.InstanceName = "JourneyFinder_";
+        });
+
+        services.Configure<ForwardedHeadersOptions>(options =>
+        {
+            options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+            options.KnownProxies.Add(IPAddress.Parse("127.0.0.1"));
+        });
+
+        return services;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,79 +1,38 @@
-using System.Net;
-using JourneyFinder.Builders;
-using JourneyFinder.Factories;
+using JourneyFinder.Extensions;
 using JourneyFinder.Filters;
-using JourneyFinder.Helpers;
-using JourneyFinder.Managers;
-using JourneyFinder.Managers.Interfaces;
-using JourneyFinder.Options;
-using JourneyFinder.Services;
-using JourneyFinder.Services.Interfaces;
-using JourneyFinder.Settings;
-using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddHttpClient();
-builder.Services.AddScoped<ISessionService, SessionService>();
-builder.Services.AddScoped<IBusLocationService, BusLocationService>();
-builder.Services.AddScoped<IJourneyService, JourneyService>();
-builder.Services.AddScoped<ISessionManager, SessionManager>();
-builder.Services.AddScoped<IRedisCacheService, RedisCacheService>();
-builder.Services.AddScoped<IJourneyManager, JourneyManager>();
-builder.Services.AddScoped<ICookieManager, CookieManager>();
-builder.Services.AddScoped<IRequestContextHelper, RequestContextHelper>();
-builder.Services.AddScoped<IDeviceRequestFactory, DeviceRequestFactory>();
-builder.Services.AddScoped<IBusLocationManager, BusLocationManager>();
-builder.Services.AddScoped<IHomeViewModelBuilder, HomeViewModelBuilder>();
-builder.Services.AddScoped<IJourneyViewModelBuilder, JourneyViewModelBuilder>();
-builder.Services.AddScoped<JourneyValidationFilter>();
-builder.Services.Configure<ForwardedHeadersOptions>(options =>
-{
-    options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
-    options.KnownProxies.Add(IPAddress.Parse("127.0.0.1")); 
-});
-
-builder.Services.AddStackExchangeRedisCache(options =>
-{
-    options.Configuration = builder.Configuration.GetConnectionString("Redis");
-    options.InstanceName = "JourneyFinder_"; 
-});
-
-builder.Services.AddSession(options =>
-{
-    options.Cookie.Name = ".JourneyFinder.Session";
-    options.Cookie.HttpOnly = true;
-    options.Cookie.IsEssential = true;
-    options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
-    options.IdleTimeout = TimeSpan.FromDays(30); 
-});
-
-builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
-builder.Services.Configure<ObiletApiOptions>(
-    builder.Configuration.GetSection("ObiletApi"));
-builder.Services.Configure<CookieSettings>(builder.Configuration.GetSection("CookieSettings"));
-
-builder.Services.AddControllersWithViews(o =>
+builder.Services
+    .AddHttpClient()
+    .AddCustomServices()
+    .AddCustomManagers()
+    .AddCustomBuilders()
+    .AddCustomFactories()
+    .AddCustomHelpers()
+    .AddCustomFilters()
+    .AddCustomConfiguration(builder.Configuration)
+    .AddCustomLocalization()
+    .AddCustomRedis(builder.Configuration)
+    .AddSession(options =>
     {
-        o.Filters.Add<PersistentCookieActionFilter>();
+        options.Cookie.Name = ".JourneyFinder.Session";
+        options.Cookie.HttpOnly = true;
+        options.Cookie.IsEssential = true;
+        options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+        options.IdleTimeout = TimeSpan.FromDays(30);
+    });
+
+builder.Services.AddControllersWithViews(options =>
+    {
+        options.Filters.Add<PersistentCookieActionFilter>();
     })
     .AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix)
     .AddDataAnnotationsLocalization();
 
-var supportedCultures = new[] { "tr-TR", "en-EN" };
-
-builder.Services.Configure<RequestLocalizationOptions>(options =>
-{
-    options.DefaultRequestCulture = new Microsoft.AspNetCore.Localization.RequestCulture("tr-TR");
-    options.SupportedCultures = supportedCultures.Select(c => new System.Globalization.CultureInfo(c)).ToList();
-    options.SupportedUICultures = supportedCultures.Select(c => new System.Globalization.CultureInfo(c)).ToList();
-});
-
 var app = builder.Build();
-app.UseRequestLocalization();
-app.UseForwardedHeaders();
 
 if (!app.Environment.IsDevelopment())
 {
@@ -84,10 +43,10 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseRouting();
 app.UseSession();
-
 app.UseAuthorization();
-var locOptions = app.Services.GetRequiredService<IOptions<RequestLocalizationOptions>>();
-app.UseRequestLocalization(locOptions.Value);
+
+app.UseForwardedHeaders();
+app.UseRequestLocalization(app.Services.GetRequiredService<IOptions<RequestLocalizationOptions>>().Value);
 app.MapStaticAssets();
 
 app.MapControllerRoute(
